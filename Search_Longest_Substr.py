abec922c314e81d32a9a194604824a7fa459e871
Dchment <37564522+Dchment@users.noreply.github.com>
2020-12-05 12:29:32 +0800

    Add files via upload

---
+++ Search_Longest_Substr.py
@@ -0,0 +1,21 @@
+#return the specified string's longest substring and its length
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        sub=''
+        sublen=0
+        for i in range(len(s)):
+            sub_temp=''
+            sublen_temp=0
+            j=i
+            while ((j<len(s) and (s[j] not in sub_temp))):
+                sub_temp+=s[j]
+                sublen_temp+=1
+                j+=1
+            if sublen_temp>sublen:
+                sublen=sublen_temp
+                sub=sub_temp
+        return sub,sublen
+
+if __name__ == "__main__":
+    s=Solution()
+    print(s.lengthOfLongestSubstring('abcabcbb'))
